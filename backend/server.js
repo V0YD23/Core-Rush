e2a94927e4a8d84022c341004a2c847b4acf70db
yash <slayycasm997@gmail.com>
2025-02-22 08:00:48 +0530

    added nodemon to package.json

--- backend/server.js
+++ backend/server.js
@@ -342,8 +342,8 @@ app.post("/generate-proof", async (req, res) => {
     }
 
     // Load the WebAssembly and witness calculator
-    const wasmPath = "build/game_js/game.wasm";
-    const zkeyPath = "build/game.zkey";
+    const wasmPath = "builds/game_js/game.wasm";
+    const zkeyPath = "builds/game.zkey";
     const input = { finalScore };
 
     // Generate witness
